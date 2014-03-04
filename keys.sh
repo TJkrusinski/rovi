6bd2a87758bfd5aba0566aa3aefcc090f4e0563f
TJ Krusinski <tjkrus@gmail.com>
2014-03-04 11:25:37 -0500

    adds keys.sh example

---
+++ keys.sh
@@ -0,0 +1,11 @@
+# auto complete
+export ROVI_AUTO_KEY="Your Key"
+export ROVI_ATUO_SECRET="Your Key"
+
+# metadata and search
+export ROVI_META_KEY="Your Key"
+export ROVI_META_SECRET="Your Key"
+
+# TV Listing
+export ROVI_TV_KEY="Your Key"
+export ROVI_TV_SECRET="Your Key"
